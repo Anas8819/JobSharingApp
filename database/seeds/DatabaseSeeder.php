b1efec4f4e05abdf2d1c95f3fe8dde70d8fe6114
Anas8819 <anasahmed8819@hotmail.com>
2017-11-15 20:36:12 +0500

    Working with login and register

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,18 +12,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Let's truncate our existing records to start from scratch.
-        \App\User::truncate();
+
 
         $faker = \Faker\Factory::create();
 
         // And now, let's create a few articles in our database:
+//        for ($i = 0; $i < 50; $i++) {
+//            \App\User::create([
+//                'name' => $faker->sentence,
+//                'email' => $faker->email,
+//                'password' => $faker->password(8, 15),
+//                'api_token' => $faker->name('Jll7q0BSijLOrzaOSm5Dr5hW9cJRZAJKOzvDlxjKCXepwAeZ7JR6YP5zQqnw')
+//            ]);
+//        }
         for ($i = 0; $i < 50; $i++) {
-            \App\User::create([
-                'name' => $faker->sentence,
-                'email' => $faker->email,
-                'password' => $faker->password(8,15),
-                'role' => $faker->name('A'),
-                'api_token' => $faker->name('Jll7q0BSijLOrzaOSm5Dr5hW9cJRZAJKOzvDlxjKCXepwAeZ7JR6YP5zQqnw')
+            \App\Job::create([
+                'title' => $faker->sentence,
+                'description' => $faker->sentence,
+                'organization' => $faker->sentence,
+                'type' => $faker->word('U'),
+                'salary' => $faker->numberBetween(10000,50000),
+                'date' => $faker->date('1996-08-26')
             ]);
         }
     }
